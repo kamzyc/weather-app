88f6252570dd102e8999420adddb995a5863c112
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-10-05 20:20:30 +0200

    add basic fetch data function

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 "use strict";
 
-const title = document.querySelector(".title");
+import { API_URL, API_KEY } from "./config";
 
-title.textContent = "I'm from JS!";
+const searchInput = document.querySelector(".search__input");
+const searchBtn = document.querySelector(".search__btn");
+
+const getData = async (city) => {
+   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+   try {
+      const data = await fetch(`${API_URL}q=${city}&appid=${API_KEY}`);
+      const weatherData = await data.json();
+      console.log(weatherData);
+   } catch (err) {
+      console.error(err);
+   }
+};
+
+getData("Paris");
