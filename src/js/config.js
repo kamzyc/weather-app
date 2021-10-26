2f03e8ad10f82c365d628b9505538c2588d5a777
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-10-26 19:08:21 +0200

    rename icons

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,37 @@
 import dotenv from "dotenv";
 
-import clearSkyDayIcon from "url:../imgs/icons/800d.svg";
-import clearSkyNightIcon from "url:../imgs/icons/800n.svg";
+// clear sky
+import i800d from "url:../imgs/icons/800d.svg";
+import i800n from "url:../imgs/icons/800n.svg";
+
+// clouds
+import i801d from "url:../imgs/icons/801d.svg";
+import i801n from "url:../imgs/icons/801n.svg";
+import i802 from "url:../imgs/icons/802.svg";
+import i803_804 from "url:../imgs/icons/803_804.svg";
+
+// atmosphere
+import i700 from "url:../imgs/icons/700.svg";
+
+// snow
+import i600_601_620_621d from "url:../imgs/icons/600_601_620_621d.svg";
+import i600_601_620_621n from "url:../imgs/icons/600_601_620_621n.svg";
+import i602_622 from "url:../imgs/icons/602_622.svg";
+import i611_612_613_615_616 from "url:../imgs/icons/611_612_613_615_616.svg";
+
+// rain
+import i500_501_520_521d from "url:../imgs/icons/500_501_520_521d.svg";
+import i500_501_520_521n from "url:../imgs/icons/500_501_520_521n.svg";
+import i502_503_504_522_531 from "url:../imgs/icons/502_503_504_522_531.svg";
+import i511 from "url:../imgs/icons/511.svg";
+
+// drizzle
+import i300 from "url:../imgs/icons/300.svg";
+
+// thuderstorm
+import i210_211_230 from "url:../imgs/icons/210_211_230.svg";
+import i200_201_202_231_232 from "url:../imgs/icons/200_201_202_231_232.svg";
+import i212_221 from "url:../imgs/icons/212_221.svg";
 
 dotenv.config();
 export const API_KEY = process.env.API_KEY;
@@ -24,7 +54,7 @@ export const DATE_OPTIONS = {
 };
 
 export const ICONS = {
-   800: clearSkyDayIcon,
+   800: i800d,
 };
 
 /*
