65fc0bcbfb7ee6821103d0243e8d3c425d4fdadb
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-10-27 20:59:56 +0200

    finish current weather

--- src/js/view.js
+++ src/js/view.js
@@ -12,122 +12,122 @@ export const pinBtn = document.querySelector(".navbar__pin");
 const mainWeatherContainer = document.querySelector(".current-weather");
 const hourlyContainer = document.querySelector(".hourly");
 
-//^ MAIN ELEMENT
-const createTitleElem = (name) => {
-   // return createElem("h2", "current-weather__location", name);
-   return DOMCreator.createElement("h2", "current-weather__location", name);
-};
+// //^ MAIN ELEMENT
+// const createTitleElem = (name) => {
+//    // return createElem("h2", "current-weather__location", name);
+//    return DOMCreator.createElement("h2", "current-weather__location", name);
+// };
 
-const createDateElem = () => {
-   const date = convertDate(new Date(), DATE_OPTIONS.LONG);
-   // return createElem("div", "current-weather__date", date);
-   return DOMCreator.createElement("div", "current-weather__date", date);
-};
+// const createDateElem = () => {
+//    const date = convertDate(new Date(), DATE_OPTIONS.LONG);
+//    // return createElem("div", "current-weather__date", date);
+//    return DOMCreator.createElement("div", "current-weather__date", date);
+// };
 
-const createIconElem = (className, id, sunrise, sunset) => {
-   const parent = DOMCreator.createElement("div", className);
-   const icon = DOMCreator.createElement("img");
+// const createIconElem = (className, id, sunrise, sunset) => {
+//    const parent = DOMCreator.createElement("div", className);
+//    const icon = DOMCreator.createElement("img");
 
-   const dayTime = checkDayTime(sunrise, sunset);
-   const iconType = checkIcon(id, dayTime);
+//    const dayTime = checkDayTime(sunrise, sunset);
+//    const iconType = checkIcon(id, dayTime);
 
-   // addAttribute(icon, "src", ICONS[`${iconType}`]);
-   DOMCreator.addAttribute(icon, "src", ICONS[`${iconType}`]);
+//    // addAttribute(icon, "src", ICONS[`${iconType}`]);
+//    DOMCreator.addAttribute(icon, "src", ICONS[`${iconType}`]);
 
-   // addElem(icon, parent);
-   DOMCreator.appendElements(icon, parent);
-   return parent;
-};
+//    // addElem(icon, parent);
+//    DOMCreator.appendElements(icon, parent);
+//    return parent;
+// };
 
-const createMaxMinElem = (max, min) => {
-   const parent = DOMCreator.createElement(
-      "div",
-      "current-weather__min-max",
-      ""
-   );
-   const tempMaxElem = DOMCreator.createElement("span", "max", `${max}°`);
-   const tempMinElem = DOMCreator.createElement("span", "min", ` / ${min}°`);
-   DOMCreator.appendElements([tempMaxElem, tempMinElem], parent);
-   return parent;
-};
+// const createMaxMinElem = (max, min) => {
+//    const parent = DOMCreator.createElement(
+//       "div",
+//       "current-weather__min-max",
+//       ""
+//    );
+//    const tempMaxElem = DOMCreator.createElement("span", "max", `${max}°`);
+//    const tempMinElem = DOMCreator.createElement("span", "min", ` / ${min}°`);
+//    DOMCreator.appendElements([tempMaxElem, tempMinElem], parent);
+//    return parent;
+// };
 
-const createWeatherElem = (temp, description, id, sunrise, sunset) => {
-   const tempElem = DOMCreator.createElement(
-      "div",
-      "current-weather__temp",
-      `${temp.main}°`
-   );
-   const descriptionElem = DOMCreator.createElement(
-      "div",
-      "current-weather__description",
-      description
-   );
-   const iconElem = createIconElem(
-      "current-weather__icon",
-      id,
-      sunrise,
-      sunset
-   );
-   const maxMinSection = createMaxMinElem(temp.max, temp.min);
+// const createWeatherElem = (temp, description, id, sunrise, sunset) => {
+//    const tempElem = DOMCreator.createElement(
+//       "div",
+//       "current-weather__temp",
+//       `${temp.main}°`
+//    );
+//    const descriptionElem = DOMCreator.createElement(
+//       "div",
+//       "current-weather__description",
+//       description
+//    );
+//    const iconElem = createIconElem(
+//       "current-weather__icon",
+//       id,
+//       sunrise,
+//       sunset
+//    );
+//    const maxMinSection = createMaxMinElem(temp.max, temp.min);
 
-   const parent = DOMCreator.createElement(
-      "section",
-      "current-weather__content"
-   );
+//    const parent = DOMCreator.createElement(
+//       "section",
+//       "current-weather__content"
+//    );
 
-   DOMCreator.appendElements(
-      [tempElem, iconElem, descriptionElem, maxMinSection],
-      parent
-   );
-   return parent;
-};
+//    DOMCreator.appendElements(
+//       [tempElem, iconElem, descriptionElem, maxMinSection],
+//       parent
+//    );
+//    return parent;
+// };
 
-const createMainElem = (
-   { name },
-   { temp, description, id, sunrise, sunset }
-) => {
-   const titleElem = createTitleElem(name);
-   const dateElem = createDateElem();
-   const weatherDataElem = createWeatherElem(
-      temp,
-      description,
-      id,
-      sunrise,
-      sunset
-   );
+// const createMainElem = (
+//    { name },
+//    { temp, description, id, sunrise, sunset }
+// ) => {
+//    const titleElem = createTitleElem(name);
+//    const dateElem = createDateElem();
+//    const weatherDataElem = createWeatherElem(
+//       temp,
+//       description,
+//       id,
+//       sunrise,
+//       sunset
+//    );
 
-   return [titleElem, dateElem, weatherDataElem];
-};
+//    return [titleElem, dateElem, weatherDataElem];
+// };
 
-//^ hOURLY ELEMENT
-const createHourElem = ({ time, id, temp }) => {
-   const timeElem = DOMCreator.createElement(
-      "div",
-      "hourly__time",
-      convertDate(time, DATE_OPTIONS.ONLY_TIME)
-   );
-   const iconElem = DOMCreator.createElement("div", "hourly__icon", "X");
+// //^ hOURLY ELEMENT
+// const createHourElem = ({ time, id, temp }) => {
+//    const timeElem = DOMCreator.createElement(
+//       "div",
+//       "hourly__time",
+//       convertDate(time, DATE_OPTIONS.ONLY_TIME)
+//    );
+//    const iconElem = DOMCreator.createElement("div", "hourly__icon", "X");
 
-   const tempElem = DOMCreator.createElement("div", "hourly__temp", temp);
+//    const tempElem = DOMCreator.createElement("div", "hourly__temp", temp);
 
-   const hourElem = DOMCreator.createElement("li", "hourly__item");
-   DOMCreator.appendElements([timeElem, iconElem, tempElem], hourElem);
-   return hourElem;
-};
+//    const hourElem = DOMCreator.createElement("li", "hourly__item");
+//    DOMCreator.appendElements([timeElem, iconElem, tempElem], hourElem);
+//    return hourElem;
+// };
 
-const createHoursElem = (hourly) => {
-   const hoursElem = hourly.map(createHourElem);
-   return hoursElem;
-};
+// const createHoursElem = (hourly) => {
+//    const hoursElem = hourly.map(createHourElem);
+//    return hoursElem;
+// };
 
-const createHourlyElem = (hourly) => {
-   const listElem = DOMCreator.createElement("ul", "hourly__list");
-   const hoursElem = createHoursElem(hourly);
+// const createHourlyElem = (hourly) => {
+//    const listElem = DOMCreator.createElement("ul", "hourly__list");
+//    const hoursElem = createHoursElem(hourly);
 
-   DOMCreator.appendElements(hoursElem, listElem);
+//    DOMCreator.appendElements(hoursElem, listElem);
 
-   return listElem;
-};
+//    return listElem;
+// };
 
 // export const updateView = (location, currentWeather, hourly) => {
 //    console.log("Updating view...");
@@ -148,23 +148,9 @@ const createHourlyElem = (hourly) => {
 //    DOMCreator.appendElements(hourlyElem, hourlyContainer);
 // };
 
-// const createIconElem = (className, id, sunrise, sunset) => {
-//    const parent = DOMCreator.createElement("div", className);
-//    const icon = DOMCreator.createElement("img");
-
-//    const dayTime = checkDayTime(sunrise, sunset);
-//    const iconType = checkIcon(id, dayTime);
-
-//    // addAttribute(icon, "src", ICONS[`${iconType}`]);
-//    DOMCreator.addAttribute(icon, "src", ICONS[`${iconType}`]);
-
-//    // addElem(icon, parent);
-//    DOMCreator.appendElements(icon, parent);
-//    return parent;
-// };
-
 // new version
 
+//////////////////////////////////////////////////////////////////////////////////////////
 //? Main weather element
 const createIconElem = (
    id,
@@ -172,10 +158,34 @@ const createIconElem = (
    sunset,
    className = CLASSNAMES.CURRENT_WEATHER
 ) => {
-   const parent = DOMCreator.createElement("");
+   const parent = DOMCreator.createElement("div", `${className}__icon`);
+   const icon = DOMCreator.createElement("img");
+
+   const dayTime = checkDayTime(sunrise, sunset);
+   const iconType = checkIcon(id, dayTime);
+
+   DOMCreator.addAttribute(icon, "src", ICONS[`${iconType}`]);
+
+   DOMCreator.appendElements(icon, parent);
+   return parent;
 };
 
-const createContentElement = ({ temp, id }, { sunrise, sunset }) => {
+const createMinMaxElement = ({ min, max }) => {
+   const parent = DOMCreator.createElement(
+      "div",
+      `${CLASSNAMES.CURRENT_WEATHER}__min-max`
+   );
+   const maxElemement = DOMCreator.createElement("span", "max", `${max}°`);
+   const minElement = DOMCreator.createElement("span", "min", ` / ${min}°`);
+
+   DOMCreator.appendElements([maxElemement, minElement], parent);
+   return parent;
+};
+
+const createContentElement = (
+   { temp, id, description },
+   { sunrise, sunset }
+) => {
    const parent = DOMCreator.createElement(
       "section",
       `${CLASSNAMES.CURRENT_WEATHER}__content`
@@ -192,10 +202,20 @@ const createContentElement = ({ temp, id }, { sunrise, sunset }) => {
    const iconElement = createIconElem(id, sunrise, sunset);
 
    // description
+   const descriptionElement = DOMCreator.createElement(
+      "div",
+      `${CLASSNAMES.CURRENT_WEATHER}__description`,
+      description
+   );
+
    // min-max
+   const minMaxElement = createMinMaxElement(temp);
 
    // add to parent
-   DOMCreator.appendElements([tempElement], parent);
+   DOMCreator.appendElements(
+      [tempElement, iconElement, descriptionElement, minMaxElement],
+      parent
+   );
    return parent;
 };
 
@@ -231,6 +251,12 @@ const createMainWeatherElement = ({ name, lat, lon }, timeData, weather) => {
    );
    return parent;
 };
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//? Hourly element
+//////////////////////////////////////////////////////////////////////////////////////////
+const createHourlyElement = () => {};
+//////////////////////////////////////////////////////////////////////////////////////////
 
 export const updateView = (location, timeData, weather) => {
    console.log("Updating view...");
@@ -243,6 +269,7 @@ export const updateView = (location, timeData, weather) => {
       weather
    );
 
+   const hourlyElement = createHourlyElement();
    // clear containers
    DOMCreator.clearElement(mainWeatherContainer);
 
